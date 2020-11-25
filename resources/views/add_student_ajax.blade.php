d70623804776822ddc8e52eb1a4b6e849ba22096
shijin <shijinsubramanniam@gmail.com>
2020-11-25 15:26:05 +0530

    changed

--- resources/views/add_student_ajax.blade.php
+++ resources/views/add_student_ajax.blade.php
@@ -41,7 +41,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Student form</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Students form</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
